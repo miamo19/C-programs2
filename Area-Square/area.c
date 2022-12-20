de324dfaf917a5cb105df9b7370edd9ab1abee6d
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-21 00:22:44 +0100

    Update area.c

--- Area-Square/area.c
+++ Area-Square/area.c
@@ -2,10 +2,10 @@
 #include<conio.h>
 int main()
 {
-    float len, area;
-    printf("Enter one side of the Square: ");
-    scanf("%f", &len);
-    areas = len*len;
+    float side, area;
+    printf("Enter the lenght of one side of the Square: ");
+    scanf("%f", &side);
+    areas = side*side;
     printf("\nArea = %0.2f", area);
     getch();
     return 0;
