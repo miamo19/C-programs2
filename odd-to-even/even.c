79bf8552f6c65f6720098f17f1a47c618d8baf1b
Hyacinthe <hyacinthemiamo5@gmail.com>
2022-12-20 05:07:00 +0100

    Update even.c

--- odd-to-even/even.c
+++ odd-to-even/even.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main() {
     int num;
-    printf("Enter an integer: ");
+    int num2;
+    printf("Enter an first integer: ");
+    scanf("%d", &num);
+    
+    printf("Enter an second integer: ");
     scanf("%d", &num);
     
     (num % 2 == 0) ? printf("%d is even.", num) : printf("%d is odd.", num);
